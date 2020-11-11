57832762182034cdd8a89b10c361615ec98543c4
thetherington <thomas@hetheringtons.org>
2020-11-11 11:55:24 -0500

    initial poller_config settings

---
+++ scripts/poller_config.py
@@ -0,0 +1,35 @@
+import json
+from MediatorFolderMonitor import FolderFetcher
+from insite_plugin import InsitePlugin
+
+
+class Plugin(InsitePlugin):
+    def can_group(self):
+        return False
+
+    def fetch(self, hosts):
+
+        try:
+
+            self.collector
+
+        except Exception:
+
+            host = hosts[-1]
+
+            params = {
+                "host": host,
+                "port": "8080",
+            }
+
+            self.collector = FolderFetcher(**params)
+
+        documents = []
+
+        for metrics in self.collector.generate_stats():
+
+            document = {"fields": metrics, "host": metrics["s_host_name"], "name": "folders"}
+
+            documents.append(document)
+
+        return json.dumps(documents)
